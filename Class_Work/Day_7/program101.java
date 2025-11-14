4dacf0eaa08e35c6418d08ab8af64f2a09a1a0e3
Sakshib20 <154331302+Sakshib20@users.noreply.github.com>
2025-11-14 18:24:49 +0530

    Added Classwork Day7

---
+++ Class_Work/Day_7/program101.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+class ArrayX
+{
+    public void Display(int Brr[])
+    {
+        int i=0;
+
+        System.out.println("Elements of the Array are : ");
+        for(i=0;i<Brr.length;i++)
+        {
+            System.out.println(Brr[i]);
+        }
+    }
+
+    public int COuntEVen(int Brr[])
+    {
+        int i=0,iCount=0;
+
+        for(i=0;i<Brr.length;i++)
+        {
+            if((Brr[i]%2)==0)
+            {
+                iCount++;
+            }
+        }
+
+        return iCount;
+    }
+}
+
+class program101
+{
+    public static void main(String A[])
+        
+    {
+        Scanner sobj=new Scanner(System.in);
+
+        int iSize=0, i=0, iRet=0;
+        
+        System.out.println("Enter the size of Array : ");
+        iSize=sobj.nextInt();
+
+        int Arr[]=new int[iSize];
+
+        System.out.println("Enter the elements : ");
+        
+        for(i=0;i<Arr.length;i++)
+        {
+        Arr[i]=sobj.nextInt();
+        }
+
+        ArrayX aobj=new ArrayX();
+
+        aobj.Display(Arr);
+
+        iRet=aobj.COuntEVen(Arr);
+        System.out.println("Total Even elements are: "+ iRet);
+
+        //Important
+
+        aobj=null;
+        Arr=null;
+        sobj=null;
+
+        System.gc();
+    }
+}
